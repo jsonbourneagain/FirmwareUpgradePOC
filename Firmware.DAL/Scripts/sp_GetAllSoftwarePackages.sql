ac7e9081c3a3f68859d72ed3488b90d3532185f5
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-11-21 15:08:48 +0530

    Updated pagination logic

--- Firmware.DAL/Scripts/sp_GetAllSoftwarePackages.sql
+++ Firmware.DAL/Scripts/sp_GetAllSoftwarePackages.sql
@@ -1,24 +1,17 @@
 CREATE PROCEDURE Inventory.usp_GetAllSoftwarePackages(@PageNo INT, @PageSize INT)
---@SwPkgUID UNIQUEIDENTIFIER OUT, @SwPkgVersion VARCHAR (100) OUT, @SwColorStandardID	INT OUT, @SwAddedDate DATETIME OUT, @SwFileName NVARCHAR (200) OUT, @SwFileSize VARCHAR (30) OUT 
+
 AS BEGIN 
 	SET NOCOUNT ON
 	SET XACT_ABORT ON
 
-	DECLARE @startPos INT
-	DECLARE @endPos  INT
-
-	SET @startPos = (@PageNo -1) * @PageSize + 1 
-	SET @endPos = @PageNo * @PageSize
-
-	BEGIN TRANSACTION
-
+	
 	SELECT SWPKG.SwPkgID, SWPKG.SwPkgUID , SWPKG.SwPkgVersion, SWPKG.SwColorStandardID, SWPKG.AddedDate, FD.FileName, FD.FileSize, FD.FileFormat
 	  FROM Inventory.SoftwarePackage AS SWPKG
 	  INNER JOIN 
 	  Inventory.FileDetails AS FD 
 	  ON SWPKG.SwPkgUID = FD.SwPkgUID
+	  ORDER BY SWPKG.AddedDate desc 
+      OFFSET @PageSize * (@PageNo - 1) ROWS
+      FETCH NEXT @PageSize ROWS ONLY; 
 
-	  WHERE SWPKG.SwPkgID > @startPos AND SWPKG.SwPkgID < @endPos AND FD.FileFormat = 'bin'
-
-	COMMIT
 END
