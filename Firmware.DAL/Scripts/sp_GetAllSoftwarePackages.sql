177284b3f712f0dd8fb1f1f6a32654b78e76534a
Json Bourne <mysocialmediatwitter@protonmail.com>
2019-11-28 16:27:45 +0530

    execption handled

--- Firmware.DAL/Scripts/sp_GetAllSoftwarePackages.sql
+++ Firmware.DAL/Scripts/sp_GetAllSoftwarePackages.sql
@@ -1,19 +1,83 @@
-CREATE PROCEDURE Inventory.usp_GetAllSoftwarePackages(@PageNo INT, @PageSize INT)
+CREATE PROCEDURE Inventory.usp_GetAllSoftwarePackages(@PageNo INT, @PageSize INT, @SearchText VARCHAR(100), @SortColumn VARCHAR(50), @SortDirection VARCHAR(10))
 
 AS BEGIN 
 	SET NOCOUNT ON
 	SET XACT_ABORT ON
 
-	
 	SELECT SWPKG.SwPkgUID , SWPKG.SwPkgVersion, SWPKG.SwColorStandardID, SWPKG.AddedDate, SWPKG.Manufacturer, SWPKG.DeviceType ,FD.FileName, FD.FileSize, FD.FileFormat
 	  FROM Inventory.SoftwarePackage AS SWPKG
 	  INNER JOIN 
 	  Inventory.FileDetails AS FD 
 	  ON SWPKG.SwPkgUID = FD.SwPkgUID
-	  WHERE FD.FileFormat = 'bin' AND SWPKG.IsDeleted = 0
-	  ORDER BY SWPKG.AddedDate desc 
+	  WHERE FD.FileFormat = 'bin' AND SWPKG.IsDeleted = 0 AND( FD.FileName LIKE '%' +@SearchText + '%')
+	  ORDER BY 
+		CASE 
+			 WHEN @SortDirection <> 'ASC' THEN '' 
+			 WHEN @SortColumn =  'FILE NAME' THEN FD.FileName 
+		END ASC,
+		CASE
+			 WHEN @SortDirection <> 'ASC' THEN ''
+			 WHEN @SortColumn =  'VERSION' THEN SWPKG.SwPkgVersion
+		END ASC,
+		CASE
+			 WHEN @SortDirection <> 'ASC' THEN 0
+			 WHEN @SortColumn =  'TYPE' THEN SWPKG.SwColorStandardID
+		END ASC,
+		CASE
+			 WHEN @SortDirection <> 'ASC' THEN ''
+			 WHEN @SortColumn =  'MANUFACTURER' THEN SWPKG.Manufacturer
+		END ASC,
+		CASE
+			 WHEN @SortDirection <> 'ASC' THEN ''
+			 WHEN @SortColumn =  'DEVICE' THEN SWPKG.DeviceType
+		END ASC,
+		CASE
+			 WHEN @SortDirection <> 'ASC' THEN ''
+			 WHEN @SortColumn =  'SIZE' THEN FD.FileSize
+		END ASC,
+		CASE
+			 WHEN @SortDirection <> 'ASC' THEN CAST(NULL AS DATE)
+			 WHEN @SortColumn =  'DATE ADDED' THEN SWPKG.AddedDate
+		END	 ASC,
+		
+		CASE 
+			 WHEN @SortDirection <> 'DESC' THEN '' 
+			 WHEN @SortColumn =  'FILE NAME' THEN FD.FileName 
+		END DESC,
+		CASE
+			 WHEN @SortDirection <> 'DESC' THEN ''
+			 WHEN @SortColumn =  'VERSION' THEN SWPKG.SwPkgVersion
+		END DESC,
+		CASE
+			 WHEN @SortDirection <> 'DESC' THEN 0
+			 WHEN @SortColumn =  'TYPE' THEN SWPKG.SwColorStandardID
+		END DESC,
+		CASE
+			 WHEN @SortDirection <> 'DESC' THEN ''
+			 WHEN @SortColumn =  'MANUFACTURER' THEN SWPKG.Manufacturer
+		END DESC,
+		CASE
+			 WHEN @SortDirection <> 'DESC' THEN ''
+			 WHEN @SortColumn =  'DEVICE' THEN SWPKG.DeviceType
+		END DESC,
+		CASE
+			 WHEN @SortDirection <> 'DESC' THEN ''
+			 WHEN @SortColumn =  'SIZE' THEN FD.FileSize
+		END ASC,
+		CASE
+			 WHEN @SortDirection <> 'DESC' THEN CAST(NULL AS DATE)
+			 WHEN @SortColumn =  'DATE ADDED' THEN SWPKG.AddedDate
+		END	 DESC
+
+
+	  --DESC
+	  
+	   
       OFFSET @PageSize * (@PageNo - 1) ROWS
       FETCH NEXT @PageSize ROWS ONLY;
+
+
+
 
 	  SELECT COUNT(SWPKG.SwPkgUID) AS TotalRecords
 	  FROM Inventory.SoftwarePackage AS SWPKG
@@ -34,5 +98,5 @@ AS BEGIN
 	  Inventory.SoftwarePackage SP
 	  ON
 	  SM.SwPkgUID = SP.SwPkgUID
-
+	  --WHERE SP.SwPkgUID IN()
 END
--- Firmware.WebApi/Controllers/FirmwareController.cs
+++ Firmware.WebApi/Controllers/FirmwareController.cs
@@ -25,54 +25,62 @@ namespace Firmware.WebApi.Controllers
         [HttpPost, Route("api/UploadSoftwarePackage")]
         public async Task<IHttpActionResult> UploadSoftwarePackage(string guid)
         {
-            string key = guid.Trim('\"');
-            var count = HttpContext.Current.Request.Files.Count;
-            var beforeUpload = GC.GetTotalMemory(false);
-            string id = string.Empty;
-            if (!Request.Content.IsMimeMultipartContent())
+            try
             {
-                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                string key = guid.Trim('\"');
+                var count = HttpContext.Current.Request.Files.Count;
+                var beforeUpload = GC.GetTotalMemory(false);
+                string id = string.Empty;
+                if (!Request.Content.IsMimeMultipartContent())
+                {
+                    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+                }
+                var provider = new MultipartMemoryStreamProvider();
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                byte[] fristBuffer = null, swPackgBuffer = null;
+                byte[] secondBuffer = null, helpDocBuffer = null;
+
+                string firstFileName = string.Empty, swPackgFilename = string.Empty;
+                string secondFileName = string.Empty, helpDocFilename = string.Empty;
+
+                var firstFile = provider.Contents[0];
+                firstFileName = firstFile.Headers.ContentDisposition.FileName.Trim('\"');
+                fristBuffer = await firstFile.ReadAsByteArrayAsync();
+
+                if (count > 1)
+                {
+                    var secondFile = provider.Contents[1];
+                    secondFileName = secondFile.Headers.ContentDisposition.FileName.Trim('\"');
+                    secondBuffer = await secondFile.ReadAsByteArrayAsync();
+                }
+
+                var arr = firstFileName.Split('.');
+                var extension = arr[arr.Length - 1];
+
+                if (extension == "bin" || extension == "pkg")
+                {
+                    swPackgBuffer = fristBuffer;
+                    swPackgFilename = firstFileName;
+                    helpDocBuffer = secondBuffer;
+                    helpDocFilename = secondFileName;
+                }
+                else if (extension == "pdf" || extension == "doc" || extension == "docx")
+                {
+                    swPackgBuffer = secondBuffer;
+                    swPackgFilename = secondFileName;
+                    helpDocBuffer = fristBuffer;
+                    helpDocFilename = firstFileName;
+                }
+
+                id = _repository.UploadFirmware(swPackgBuffer, swPackgFilename, helpDocBuffer, helpDocFilename, key).ToString();
+                return base.Content(HttpStatusCode.OK, id, new JsonMediaTypeFormatter(), "text/plain"); ;
             }
-            var provider = new MultipartMemoryStreamProvider();
-            await Request.Content.ReadAsMultipartAsync(provider);
-
-            byte[] fristBuffer = null, swPackgBuffer = null;
-            byte[] secondBuffer = null, helpDocBuffer = null;
-
-            string firstFileName = string.Empty, swPackgFilename = string.Empty;
-            string secondFileName = string.Empty, helpDocFilename = string.Empty;
-
-            var firstFile = provider.Contents[0];
-            firstFileName = firstFile.Headers.ContentDisposition.FileName.Trim('\"');
-            fristBuffer = await firstFile.ReadAsByteArrayAsync();
-
-            if (count > 1)
+            catch (Exception)
             {
-                var secondFile = provider.Contents[1];
-                secondFileName = secondFile.Headers.ContentDisposition.FileName.Trim('\"');
-                secondBuffer = await secondFile.ReadAsByteArrayAsync();
+                return base.Content(HttpStatusCode.OK, "Error occurred.", new JsonMediaTypeFormatter(), "text/plain"); ;
             }
-
-            var arr = firstFileName.Split('.');
-            var extension = arr[arr.Length - 1];
-
-            if (extension == "bin" || extension == "pkg")
-            {
-                swPackgBuffer = fristBuffer;
-                swPackgFilename = firstFileName;
-                helpDocBuffer = secondBuffer;
-                helpDocFilename = secondFileName;
-            }
-            else if (extension == "pdf" || extension == "doc" || extension == "docx")
-            {
-                swPackgBuffer = secondBuffer;
-                swPackgFilename = secondFileName;
-                helpDocBuffer = fristBuffer;
-                helpDocFilename = firstFileName;
-            }
-
-            id = _repository.UploadFirmware(swPackgBuffer, swPackgFilename, helpDocBuffer, helpDocFilename, key).ToString();
-            return base.Content(HttpStatusCode.OK, id, new JsonMediaTypeFormatter(), "text/plain"); ;
+            
         }
         [EnableCors(origins: "*", headers: "*", methods: "*")]
         [HttpPost, Route("api/AddSoftwarePackage")]
@@ -82,7 +90,7 @@ namespace Firmware.WebApi.Controllers
 
             var result = await Task.Run(() => _repository.AddFirmware(key, softwarePackage.SwPkgVersion, softwarePackage.SwPkgDescription, softwarePackage.SwColorStandardID, softwarePackage.SwFileChecksum, softwarePackage.SwFileChecksumType, softwarePackage.SwCreatedBy, softwarePackage.Manufacturer, softwarePackage.DeviceType, softwarePackage.SupportedModels, softwarePackage.BlobDescription));
 
-            return base.Content(HttpStatusCode.OK, true, new JsonMediaTypeFormatter(), "text/plain"); ;
+            return base.Content(HttpStatusCode.OK, result, new JsonMediaTypeFormatter(), "text/plain"); ;
         }
         [EnableCors(origins: "*", headers: "*", methods: "*")]
         [HttpPost, Route("api/CancelUpload")]
